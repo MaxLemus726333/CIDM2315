fb4215811e227353016fbbe97427654f8d4a9f8b
MaxLemus726333 <142851130+MaxLemus726333@users.noreply.github.com>
2023-08-30 18:03:55 -0500

    Homework1.cs

--- Homework1.cs
+++ Homework1.cs
@@ -6,7 +6,6 @@ class Program
     {
         double X = 2.5;
         double Y = 3.3;
-        double Z;
 
         Console.WriteLine("X = 2.5, Y = 3.3");
         Console.WriteLine($"The value of Z is: {4*X*X + 3*Y}");
