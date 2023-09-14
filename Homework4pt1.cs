00b32e9c4cb38c1bcc123d062d8ce13039c1958f
MaxLemus726333 <142851130+MaxLemus726333@users.noreply.github.com>
2023-09-14 17:58:51 -0500

    Homework4Pt1&2

---
+++ Homework4pt1.cs
@@ -0,0 +1,27 @@
+﻿namespace Homework;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Input the first number: ");
+        string a = Console.ReadLine();
+        int int_a = Convert.ToInt16(a);
+
+        Console.WriteLine("Input a second number: ");
+        string b = Console.ReadLine();
+        int int_b = Convert.ToInt16(b);
+        
+        int largest = QuestionOne(int_a, int_b);
+        Console.WriteLine($"A = {a}, B = {b}");
+        Console.WriteLine($"The largest number is: {largest}");
+    }
+    static int QuestionOne(int a, int b){
+        if(a>b){
+            return a;
+        }
+        else{
+            return b;
+        }
+    }
+}
