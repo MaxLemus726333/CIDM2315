fd548a47a113818fc08df852ae56af1034b3c24d
MaxLemus726333 <142851130+MaxLemus726333@users.noreply.github.com>
2023-09-13 21:56:56 -0500

    Homework3Pt1

---
+++ Homework3Pt1.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = 0;
+        Console.WriteLine("Input an interger: ");
+        Console.ReadLine();
+
+        while(N%2==0)
+        {
+            Console.WriteLine("N is prime");
+            break;
+        }
+        Console.WriteLine("N is non-prime");
+
+    }
+}
