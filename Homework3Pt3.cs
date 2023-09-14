971d957cc29739d0590eea2d134e3f8e6548fb18
MaxLemus726333 <142851130+MaxLemus726333@users.noreply.github.com>
2023-09-13 21:52:47 -0500

    Homework3Pt3

---
+++ Homework3Pt3.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input N: ");
+        int N = Convert.ToInt16(Console.ReadLine());
+        for(int row = 0; row<N; row++){
+            for(int col = 0; col<N; col++){
+                if(row>col){
+                Console.Write("*");
+                }
+            }
+            Console.WriteLine("*");
+        }
+    }
+}
