7ab8532d451ef300f279fdfbcc307830ac60f2ca
MaxLemus726333 <142851130+MaxLemus726333@users.noreply.github.com>
2023-09-20 21:27:51 -0500

    Homework4PT1&2

---
+++ Homework4PT2.cs
@@ -0,0 +1,30 @@
+﻿namespace Homework;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = Convert.ToInt32(Console.WriteLine("Enter an interger: "));
+        
+        string shape = Console.WriteLine("Right or Left: ");
+        Console.ReadLine();
+    }
+    static void QuestionTwo(int N, string shape){
+        if(shape == "Right"){
+            for(int i=1; i<=N; i++){
+                for(int j=1; j<=N; j++){
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+        else{
+            for(int i=1; i<=N; i++){
+                for(int j=1; j<=N; j++){
+                    Console.WriteLine("*");
+                }
+                Console.Write("");
+            }
+        }
+    }
+}
